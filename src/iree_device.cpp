d51d8fffee773e8c82078fb67aa7177ebd8b07d1
RechieKho <the.real.richie.kho@gmail.com>
2023-04-27 19:53:10 +0800

    Release device and module before capturing in `capture_*` methods in `IREEDevice`.

--- src/iree_device.cpp
+++ src/iree_device.cpp
@@ -17,6 +17,9 @@ using namespace godot;
 
 Error IREEDevice::capture_vmvx(iree_vm_instance_t* p_instance) {
     if(p_instance == nullptr) return ERR_INVALID_PARAMETER;
+
+    release();
+
     Error e = OK;
     iree_string_view_t id = iree_make_cstring_view("vmvx");
     iree_hal_sync_device_params_t params;
@@ -137,6 +140,9 @@ clean_up_driver:
 
 Error IREEDevice::capture_cpu_async(iree_vm_instance_t* p_instance) {
     if(p_instance == nullptr) return ERR_INVALID_PARAMETER;
+
+    release();
+
     Error e = OK;
     iree_string_view_t id = iree_make_cstring_view("local-task");
     iree_hal_executable_loader_t* loader = nullptr;
