8ce998f582ce3a8341b4b6db4d7b796aac92280b
RechieKho <the.real.richie.kho@gmail.com>
2023-09-13 15:58:09 +0800

    Use `vformat` instead of `String::format` in `iree_error`.

--- src/iree_error.h
+++ src/iree_error.h
@@ -1,21 +1,17 @@
 #ifndef IREE_ERROR_H
 #define IREE_ERROR_H
 
-#include <godot_cpp/classes/object.hpp>
-#include <godot_cpp/variant/string.hpp>
-#include <godot_cpp/variant/array.hpp>
+#include <iree/base/api.h>
+
+#include <godot_cpp/variant/utility_functions.hpp>
 
 #define IREE_ERR_MSG(mp_status, mp_message) \
 { \
     iree_status_t status = (mp_status); \
     if((status)) \
     { \
-        String stringified_code(iree_status_code_string(iree_status_code((status)))); \
-        String stringified_message((mp_message)); \
-        Array format_array; \
-        format_array.append(stringified_code); \
-        ERR_PRINT(stringified_message); \
-        ERR_PRINT(String("IREE code: '{1}'. IREE's error is logged to `stderr`.").format(format_array)); \
+        ERR_PRINT((mp_message)); \
+        ERR_PRINT(vformat("IREE code: '%s'. IREE's error is logged to `stderr`.", iree_status_code_string(iree_status_code(status)))); \
         iree_status_fprint(stderr, (status)); \
         iree_status_free((status)); \
         return; \
@@ -27,12 +23,8 @@
     iree_status_t status = (mp_status); \
     if((status)) \
     { \
-        String stringified_code(iree_status_code_string(iree_status_code((status)))); \
-        String stringified_message((mp_message)); \
-        Array format_array; \
-        format_array.append(stringified_code); \
-        ERR_PRINT(stringified_message); \
-        ERR_PRINT(String("IREE code: '{1}'. IREE's error is logged to `stderr`.").format(format_array)); \
+        ERR_PRINT((mp_message)); \
+        ERR_PRINT(vformat("IREE code: '%s'. IREE's error is logged to `stderr`.", iree_status_code_string(iree_status_code(status)))); \
         iree_status_fprint(stderr, (status)); \
         iree_status_free((status)); \
         return (mp_retval); \
