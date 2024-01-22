f49729b185718838b4d10268f75064c436746d8d
Rechie Kho <50512341+RechieKho@users.noreply.github.com>
2024-01-22 12:03:22 +0800

    Standardize `is_captured`. (#62)

--- src/iree_instance.cpp
+++ src/iree_instance.cpp
@@ -9,40 +9,41 @@
 
 using namespace godot;
 
-IREEInstance* IREEInstance::singleton = nullptr;
+IREEInstance *IREEInstance::singleton = nullptr;
 
-IREEInstance::IREEInstance() 
-:
-    vm_instance(nullptr),
-	device()
+IREEInstance::IREEInstance()
+    : vm_instance(nullptr),
+      device()
 {
     capture();
 }
 
-IREEInstance::~IREEInstance() {
+IREEInstance::~IREEInstance()
+{
     release();
 }
 
-Error IREEInstance::assure_vm_instance_captured() {
-    if(vm_instance != nullptr) return OK;
+Error IREEInstance::assure_vm_instance_captured()
+{
+    if (vm_instance != nullptr)
+        return OK;
 
-
-	IREE_ERR_V_MSG(
-		iree_vm_instance_create(IREE_VM_TYPE_CAPACITY_DEFAULT, iree_allocator_system(), &vm_instance),
+    IREE_ERR_V_MSG(
+        iree_vm_instance_create(IREE_VM_TYPE_CAPACITY_DEFAULT, iree_allocator_system(), &vm_instance),
         ERR_CANT_CREATE,
-		"Unable create a VM instance."
-	);
+        "Unable create a VM instance.");
 
-	IREE_ERR_V_MSG(
-		iree_hal_module_register_all_types(vm_instance),
+    IREE_ERR_V_MSG(
+        iree_hal_module_register_all_types(vm_instance),
         ERR_CANT_CREATE,
-		"Unable register HAL modules."
-	);
+        "Unable register HAL modules.");
     return OK;
 }
 
-Error IREEInstance::assure_device_captured() {
-    if(device.is_valid()) return OK;
+Error IREEInstance::assure_device_captured()
+{
+    if (device.is_captured())
+        return OK;
 
     Error error = OK;
 
@@ -54,45 +55,56 @@ Error IREEInstance::assure_device_captured() {
     return OK;
 }
 
-Error IREEInstance::capture() {
+Error IREEInstance::capture()
+{
     Error error = OK;
 
     error = assure_vm_instance_captured();
     ERR_FAIL_COND_V(error != OK, error);
-	error = assure_device_captured();
+    error = assure_device_captured();
     ERR_FAIL_COND_V(error != OK, error);
     return OK;
 }
 
-void IREEInstance::release() {
-	if(vm_instance != nullptr) {iree_vm_instance_release(vm_instance); vm_instance = nullptr;}
-	device.release();
+void IREEInstance::release()
+{
+    if (vm_instance != nullptr)
+    {
+        iree_vm_instance_release(vm_instance);
+        vm_instance = nullptr;
+    }
+    device.release();
 }
 
-IREEInstance* IREEInstance::borrow_singleton() {
-    if(IREEInstance::singleton == nullptr) IREEInstance::singleton = new IREEInstance();
+IREEInstance *IREEInstance::borrow_singleton()
+{
+    if (IREEInstance::singleton == nullptr)
+        IREEInstance::singleton = new IREEInstance();
     return IREEInstance::singleton;
 }
 
-bool IREEInstance::is_valid() const {
-    return vm_instance != nullptr && device.is_valid();
+bool IREEInstance::is_captured() const
+{
+    return vm_instance != nullptr && device.is_captured();
 }
 
-iree_vm_instance_t* IREEInstance::borrow_assured_vm_instance() {
+iree_vm_instance_t *IREEInstance::borrow_assured_vm_instance()
+{
     Error error = assure_vm_instance_captured();
     ERR_FAIL_COND_V(error != OK, nullptr);
     return vm_instance;
 }
 
-iree_hal_device_t* IREEInstance::borrow_assured_hal_device() {
+iree_hal_device_t *IREEInstance::borrow_assured_hal_device()
+{
     Error error = assure_device_captured();
     ERR_FAIL_COND_V(error != OK, nullptr);
     return device.borrow_hal_device();
 }
 
-iree_vm_module_t* IREEInstance::borrow_assured_hal_module() {
+iree_vm_module_t *IREEInstance::borrow_assured_hal_module()
+{
     Error error = assure_device_captured();
     ERR_FAIL_COND_V(error != OK, nullptr);
     return device.borrow_hal_module();
 }
-
